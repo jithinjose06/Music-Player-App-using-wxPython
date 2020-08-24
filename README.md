fe67aaa52e8b54f124ef6048ffa39c7682287c90
Jithin  Jose <jithinjose@jithins-mbp.home>
2020-08-24 18:56:37 -0400

    Added slider and Readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Music-Player-App-using-wxPython
+
+I did this project to get familiar with python GUI toolkit - wxPython.
+
+This serves as a part of another project which aims at making this app downloadable on a website and track the number of downloads.
+
+PyInstaller is used to make this app a standalone version.
+
+Just a basic music player app with the following features: Play, Pause, Load, Stop, Seek.
+
+GUI needs to be improved.
--- music_demo.py
+++ music_demo.py
@@ -1,36 +1,43 @@
 import wx
 import wx.media
 
+# Box in which all widgets are placed
 class myFrame(wx.Frame):
-	def __init__(self):
-		super().__init__(parent=None,title='Music Player')
-		panel=wx.Panel(self)
+	def __init__(self,parent=None,id=-1):
+		wx.Frame.__init__(self,parent,id,'Music Player')
+		self.panel=wx.Panel(self)
 		
 
 		mySizer=wx.BoxSizer(wx.VERTICAL)
 
-		self.mc=wx.media.MediaCtrl(panel)
+		self.mc=wx.media.MediaCtrl(self.panel)
 		mySizer.Add(self.mc,0,wx.ALL | wx.EXPAND,5)
 
-		playButton=wx.Button(panel,label='Play')
+		self.slider=wx.Slider(self.panel,-1,0,0,0,size=wx.Size(300,-1))
+		self.Bind(wx.EVT_SLIDER,self.onSeek,self.slider)
+
+		self.timer=wx.Timer(self)
+		self.Bind(wx.EVT_TIMER,self.onTimer,self.timer)
+
+		playButton=wx.Button(self.panel,-1,label='Play')
 		mySizer.Add(playButton,0,wx.ALL | wx.CENTER,5)
-		playButton.Bind(wx.EVT_BUTTON,self.onPlay)
+		self.Bind(wx.EVT_BUTTON,self.onPlay,playButton)
 
-		loadButton=wx.Button(panel,label='Load')
+		loadButton=wx.Button(self.panel,-1,label='Load')
 		mySizer.Add(loadButton,0,wx.ALL | wx.CENTER,5)
-		loadButton.Bind(wx.EVT_BUTTON,self.onLoadFile)
+		self.Bind(wx.EVT_BUTTON,self.onLoadFile,loadButton)
 
-		pauseButton=wx.Button(panel,label='Pause')
+		pauseButton=wx.Button(self.panel,-1,label='Pause')
 		mySizer.Add(pauseButton,0,wx.ALL | wx.CENTER,5)
-		pauseButton.Bind(wx.EVT_BUTTON,self.onPause)
+		self.Bind(wx.EVT_BUTTON,self.onPause,pauseButton)
 
-		stopButton=wx.Button(panel,label='Stop')
+		stopButton=wx.Button(self.panel,-1,label='Stop')
 		mySizer.Add(stopButton,0,wx.ALL | wx.CENTER,5)
-		stopButton.Bind(wx.EVT_BUTTON,self.onStop)
+		stopButton.Bind(wx.EVT_BUTTON,self.onStop,stopButton)
 
-		panel.SetSizer(mySizer)
+		self.panel.SetSizer(mySizer)
 		self.Show()
-
+	# function to load file
 	def onLoadFile(self,event):
 		wildcard="*.mp3;*wav"
 		style=wx.FD_OPEN
@@ -40,15 +47,25 @@ class myFrame(wx.Frame):
 			self.mc.Load(path)
 
 		fileDialog.Destroy()
-
+   # function to play
 	def onPlay(self,event):
 		self.mc.Play()
-
+		self.timer.Start(100)
+		self.slider.SetRange(0,self.mc.Length())
+	# function to pause
 	def onPause(self,event):
 		self.mc.Pause()
-
+    # function to stop
 	def onStop(self,event):
 		self.mc.Stop()
+    # functions for slider
+	def onTimer(self,event):
+		updateslider=self.mc.Tell()
+		self.slider.SetValue(updateslider)
+
+	def onSeek(self,event):
+		dragslider=self.slider.GetValue()
+		self.mc.Seek(dragslider)
 
 if __name__ == '__main__':
 	app = wx.App()
